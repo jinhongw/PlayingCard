632eee16b0b4d7e66208a5ce5c3d81f95698efbf
jinhongw <51617358+jinhongw@users.noreply.github.com>
2024-02-18 14:33:14 +0800

    Add public init function

--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -1,6 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 public struct PlayingCard {
-  let rank: Rank
-  let suit: Suit
+  public let rank: Rank
+  public let suit: Suit
+  
+  public init(rank: Rank, suit: Suit) {
+    self.rank = rank
+    self.suit = suit
+  }
 }
